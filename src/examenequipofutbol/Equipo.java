2191dab58c3a2736b3edf2293676f2a8479b0fd6
admin <admin@10.0.2.15>
2020-03-04 22:18:51 +0100

    Carga inicial del fichero.
    Anas Abdeselam El Farhaoui.

---
+++ src/examenequipofutbol/Equipo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+Anas Abdeselam El Farhaoui
+ */
+package examenequipofutbol;
+
+/**
+ *
+ * @author admin
+ */
+
+public class Equipo {
+    
+}
